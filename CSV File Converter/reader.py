3ad8d75d08a1a6f95af17abbb8457144369cae72
Carla <carla.debeer.uk@gmail.com>
2019-08-29 12:58:17 +0200

    Pylint code ammendments

--- CSV File Converter/reader.py
+++ CSV File Converter/reader.py
@@ -1,24 +1,26 @@
-# Carla de Beer
-# Created: July 2018
-# Converts a CVS file to a .properties file format.
-# UTF-8 character encoding is preserved.
+"""
+Carla de Beer
+Created: July 2018
+Converts a CVS file to a .properties file format.
+UTF-8 character encoding is preserved.
+"""
 
 import csv
 
-writeFile = open("i18n_en.properties", "w", newline="", encoding="utf-8")
+WRITE_FILE = open("i18n_en.properties", "w", newline="", encoding="utf-8")
 
 try:
     with open("sourceFiles/i18n_en.csv") as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=",")
-        for row in readCSV:
+        READ_CSV = csv.reader(csvfile, delimiter=",")
+        for row in READ_CSV:
             print(row[0] + "=" + row[1])
 
-            writeFile.write(row[0])
-            writeFile.write("=")
-            writeFile.write(row[1])
-            writeFile.write("\n")
+            WRITE_FILE.write(row[0])
+            WRITE_FILE.write("=")
+            WRITE_FILE.write(row[1])
+            WRITE_FILE.write("\n")
 
-    writeFile.close()
+    WRITE_FILE.close()
 
 except IOError:
-   print("Error: can\'t find file or read data")
+    print("Error: can\'t find file or read data")
--- i18n File Converter/reader.py
+++ i18n File Converter/reader.py
@@ -1,14 +1,17 @@
-# Carla de Beer
-# Created: July 2018
-# Converts .properties file to a CSV file format.
-# To ensure that special charaters are correctly displayed inside Excel, with the correct UTF-8 encoding, do the following:
-# Open Excel, import the file using UTF-encoding and set commas as delimiters.
-# Note that multiline text is not yet supported.
+"""
+Carla de Beer
+Created: July 2018
+Converts .properties file to a CSV file format.
+To ensure that special charaters are correctly displayed inside Excel,
+with the correct UTF-8 encoding, do the following:
+Open Excel, import the file using UTF-encoding and set commas as delimiters.
+Note that multiline text is not yet supported.
+"""
 
 import csv
 
-writeFile = open("i18n_en.csv", "w", newline="", encoding="utf-8")
-writer = csv.writer(writeFile)
+WRITE_FILE = open("i18n.csv", "w", newline="", encoding="utf-8")
+WRITER = csv.writer(WRITE_FILE)
 
 try:
 
@@ -25,16 +28,16 @@ try:
                 # print(line)
 
                 outgoingList = []
-                outgoingList.append("<App name>")
+                outgoingList.append("Data Disclose")
                 outgoingList.append("en")
-                outgoingList.append(parts[0]) # key
-                outgoingList.append(parts[1]) # value
+                outgoingList.append(parts[0])  # key
+                outgoingList.append(parts[1])  # value
                 myData.append(outgoingList)
 
                 print(outgoingList)
 
-                writer = csv.writer(writeFile)
-                writer.writerows(myData)
+                WRITER = csv.writer(WRITE_FILE)
+                WRITER.writerows(myData)
 
 except IOError:
-   print("Error: can\'t find file or read data")
+    print("Error: can\'t find file or read data")
