aa6eab38fe3475a8c77959e55943ee1ffa8590de
Carla <carla.debeer.uk@gmail.com>
2019-09-02 19:05:57 +0200

    Adding simple Flask example

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This repository contains a number of Python 3.7.
 
 ## List of projects
 
-* [CSV File Converter:](https://github.com/Carla-de-Beer/Python/tree/master/CSV%20File%20Converter) Converts a CSV file to a CSV file whilst preserving UTF-8 encoding
+* [CSV File Converter:](https://github.com/Carla-de-Beer/Python/tree/master/CSV%20File%20Converter) Converts a CSV file to a CSV file whilst preserving UTF-8 encoding.
 * [i18n File Converter:](https://github.com/Carla-de-Beer/Python/tree/master/i18n%20File%20Converter) Converts an i18n.properties file to a CSV file whilst preserving UTF-8 character encoding and allowing for comma-inclusion as part of the text.
-* [Letter Frequency Calculator:](https://github.com/Carla-de-Beer/Python/tree/master/Letter%20Frequency%20Calculator) Reads in a set of text files and calculates the frequency occurrences of each alphabet letter
+* [Letter Frequency Calculator:](https://github.com/Carla-de-Beer/Python/tree/master/Letter%20Frequency%20Calculator) Reads in a set of text files and calculates the frequency occurrences of each alphabet letter.
+* [Simple Book List API with Flask:](https://github.com/Carla-de-Beer/Python/tree/master/Simple%20Book%20List%20API%20with%20Flask) Simple book list API created with Python in Flask.
---
+++ Simple Book List API with Flask/book_list.py
@@ -0,0 +1,77 @@
+"""
+Book information sourced from: https://thegreatestbooks.org
+"""
+
+books = [
+    {
+        'id': 1,
+        'title': u'In Search of Lost Time',
+        'author': u'Marcel Proust',
+        'description': u'Swann\'s Way, the first part of A la recherche de temps perdu, Marcel Proust\'s seven-part cycle, was published in 1913. In it, Proust introduces the themes that run through the entire work. The narrator recalls his childhood, aided by the famous madeleine; and describes M. Swann\'s passion for Odette.',
+        'date': u'1913',
+        'read': False
+    }, {
+        'id': 2,
+        'title': u'Ulysses',
+        'author': u'James Joyce',
+        'description': u'Ulysses chronicles the passage of Leopold Bloom through Dublin during an ordinary day, June 16, 1904. The title parallels and alludes to Odysseus (Latinised into Ulysses), the hero of Homer\'s Odyssey (e.g., the correspondences between Leopold Bloom and Odysseus, Molly Bloom and Penelope, and Stephen Dedalus and Telemachus).',
+        'date': u'1922',
+        'read': True
+    }, {
+        'id': 3,
+        'title': u'Don Quixote',
+        'author': u'Miguel de Cervantes',
+        'description': u'Alonso Quixano, a retired country gentleman in his fifties, lives in an unnamed section of La Mancha with his niece and a housekeeper. He has become obsessed with books of chivalry, and believes their every word to be true, despite the fact that many of the events in them are clearly impossible.',
+        'date': u'1605',
+        'read': False
+    }, {
+        'id': 4,
+        'title': u'The Great Gatsby',
+        'author': u'F. Scott Fitzgerald',
+        'description': u'The novel chronicles an era that Fitzgerald himself dubbed the "Jazz Age". Following the shock and chaos of World War I, American society enjoyed unprecedented levels of prosperity during the "roaring" 1920s as the economy soared. At the same time, Prohibition, the ban on the sale and manufacture of alcohol as mandated by the Eighteenth Amendment, made millionaires out of bootleggers and led to an increase in organized crime, for example the Jewish mafia. Although Fitzgerald, like Nick Carraway in his novel, idolized the riches and glamor of the age, he was uncomfortable with the unrestrained materialism and the lack of morality that went with it, a kind of decadence.',
+        'date': u'1925',
+        'read': False
+    }, {
+        'id': 5,
+        'title': u'Moby Dick',
+        'author': u'Herman Melville',
+        'description': u'First published in 1851, Melville\'s masterpiece is, in Elizabeth Hardwick\'s words, "the greatest novel in American literature." The saga of Captain Ahab and his monomaniacal pursuit of the white whale remains a peerless adventure story but one full of mythic grandeur, poetic majesty, and symbolic power.',
+        'date': u'1851',
+        'read': False
+    }, {
+        'id': 6,
+        'title': u'One Hundred Years of Solitude',
+        'author': u'Gabriel Garcia Marquez',
+        'description': u'One of the 20th century\'s enduring works, One Hundred Years of Solitude is a widely beloved and acclaimed novel known throughout the world, and the ultimate achievement in a Nobel Prize–winning career. The novel tells the story of the rise and fall of the mythical town of Macondo through the history of the Buendía family.',
+        'date': u'1967',
+        'read': False
+    }, {
+        'id': 7,
+        'title': u'War and Peace',
+        'author': u'Leo Tolstoy',
+        'description': u'Epic in scale, War and Peace delineates in graphic detail events leading up to Napoleon\'s invasion of Russia, and the impact of the Napoleonic era on Tsarist society, as seen through the eyes of five Russian aristocratic families.',
+        'date': u'1865',
+        'read': True
+    }, {
+        'id': 8,
+        'title': u'Hamlet',
+        'author': u'William Shakespeare',
+        'description': u'The Tragedy of Hamlet, Prince of Denmark, or more simply Hamlet, is a tragedy by William Shakespeare, believed to have been written between 1599 and 1601. The play, set in Denmark, recounts how Prince Hamlet exacts revenge on his uncle Claudius, who has murdered Hamlet\'s father, the King, and then taken the throne and married Gertrude, Hamlet\'s mother. The play vividly charts the course of real and feigned madness—from overwhelming grief to seething rage—and explores themes of treachery, revenge, incest, and moral corruption.',
+        'date': u'1599',
+        'read': False
+    }, {
+        'id': 9,
+        'title': u'Lolita',
+        'author': u'Vladimir Nabokov',
+        'description': u'The book is internationally famous for its innovative style and infamous for its controversial subject: the protagonist and unreliable narrator, middle aged Humbert Humbert, becomes obsessed and sexually involved with a twelve-year-old girl named Dolores Haze.',
+        'date': u'1955',
+        'read': False
+    }, {
+        'id': 10,
+        'title': u'The Brothers Karamazov',
+        'author': u'Fyodor Dostoyevsky',
+        'description': u'Dostoevsky\'s last and greatest novel, The Karamazov Brothers, is both a brilliantly told crime story and a passionate philosophical debate. The dissolute landowner Fyodor Pavlovich Karamazov is murdered; his sons — the atheist intellectual Ivan, the hot-blooded Dmitry, and the saintly novice Alyosha — are all at some level involved. Bound up with this intense family drama is Dostoevsky\'s exploration of many deeply felt ideas about the existence of God, the question of human freedom, the collective nature of guilt, the disastrous consequences of rationalism.',
+        'date': u'1880',
+        'read': False
+    }
+]
