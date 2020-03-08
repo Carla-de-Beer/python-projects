a68c6500ad3cf8863aec041f3dc85c419a6eba39
Carla de Beer <carla.debeer.uk@gmail.com>
2020-03-08 12:32:37 +0100

    Fix issue with model not loading when running outside Google Colab

--- flask-projects/keras-model-deployment/api-iris-flower-dataset-classifier/deployment.py
+++ flask-projects/keras-model-deployment/api-iris-flower-dataset-classifier/deployment.py
@@ -1,17 +1,19 @@
 # Carla de Beer
 # Created: March 2020
-# A Flask API project deploying a Keras-based model based on the iris flower multivariate data, 
+# A Flask API project deploying a Keras-based model based on the iris flower multivariate data,
 # to predict an iris category based on the input parameters provided.
 # Based on the Udemy course: Complete TensorFlow 2 and Keras Deep Learning Bootcamp:
 # https://www.udemy.com/course/complete-tensorflow-2-and-keras-deep-learning-bootcamp
 # The project uses the iris flower dataset obtained from the UCI Machine Learning Repository:
 # https://archive.ics.uci.edu/ml/datasets/Iris
 
+
 from flask import Flask, request, jsonify
-import numpy as np
 from tensorflow.keras.models import load_model
+import numpy as np
 import joblib
 
+# Load the model and the scaler
 flower_model = load_model('model/final_iris_model.h5')
 flower_scaler = joblib.load('model/iris_scaler.pkl')
 
@@ -26,22 +28,24 @@ def return_prediction(model, scaler, sample_json):
     flower = scaler.transform(flower)
 
     classes = np.array(['setosa', 'versicolor', 'virginica'])
-    class_index = model.predict_classes(flower)
+    class_ind = model.predict_classes(flower)
 
-    return classes[class_index[0]]
+    return classes[class_ind][0]
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return '<h1>Flask app is running</h1>'
+    return '<h1>FLASK APP IS RUNNING!</h1>'
 
 
-@app.route("/api/flower", methods=["POST"])
-def flower_prediction():
-    return jsonify(return_prediction(flower_model, flower_scaler, request.json))
+@app.route('/api/flower', methods=['POST'])
+def predict_flower():
+    results = return_prediction(model=flower_model, scaler=flower_scaler, sample_json=request.json)
+
+    return jsonify(results)
 
 
 if __name__ == '__main__':
